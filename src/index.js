34a14a6cc3f2e74097fa405b24f48602c084e8ce
stuszken <marta.pastuszka.mp@gmail.com>
2020-11-17 21:53:00 +0100

    todo task, comments added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let slides = [...document.querySelectorAll("figure")];
 let pins = [...document.querySelectorAll(".pin")];
 let actualSlide = 0;
 
+//todo: actual pin highlighted when active
+
 //While clicking pin slide changes
 const changeSlideByPin = function (e) {
   slides[actualSlide].classList.remove("show");
@@ -35,14 +37,15 @@ const changeSlideByArrow = function (e) {
   }
 };
 
-//While clicking 'play' button, slides change every 3 sec.
+// While clicking 'play' button, slides change every 3 sec.
+// TODO stop timer when pin/arrow is clicked
 let playing = true;
 let timer;
 const autoPlay = function (e) {
   if (playing) {
     playing = false;
     e.target.classList.add("on");
-    timer = setInterval(changeSlideByTimer, 1000);
+    timer = setInterval(changeSlideByTimer, 3000);
   } else {
     playing = true;
     e.target.classList.remove("on");
@@ -51,7 +54,7 @@ const autoPlay = function (e) {
 };
 
 //function changing slides while play button clicked
-// to do: how to pass a parameter to changeSlideByArrow function - now it throws a bug
+// TODO: how to pass a parameter to changeSlideByArrow function - now it throws a bug
 // because I refer to e. which in cSBA function is mouseEvent.
 const changeSlideByTimer = function () {
   slides[actualSlide].classList.remove("show");
