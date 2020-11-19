2af3360aeeecac523210d7252f2f87d41b4ffd92
stuszken <marta.pastuszka.mp@gmail.com>
2020-11-19 21:12:25 +0100

    Pins highlighed when slide active

--- src/index.js
+++ src/index.js
@@ -4,34 +4,44 @@ let slides = [...document.querySelectorAll("figure")];
 let pins = [...document.querySelectorAll(".pin")];
 let actualSlide = 0;
 
-//todo: actual pin highlighted when active
-
 //While clicking pin slide changes
 const changeSlideByPin = function (e) {
   slides[actualSlide].classList.remove("show");
+  pins[actualSlide].classList.remove("selected");
   actualSlide = e.target.dataset.key;
   slides[actualSlide].classList.add("show");
+  pins[actualSlide].classList.add("selected");
 };
 
 //While clicking arrows slides change forward/bacward
 const changeSlideByArrow = function (e) {
-  switch (e.target.dataset.key) {
+  //I use changeSlideByArrow instead of changeSlideByTimer for Autoplay
+  // Solved: how to pass a parameter to changeSlideByArrow function - now it throws a bug
+  // because I refer to e. which in cSBA function is mouseEvent.
+  let tmp;
+  if (e === undefined) tmp = "next";
+  else tmp = e.target.dataset.key;
+  switch (tmp) {
     case "next":
       slides[actualSlide].classList.remove("show");
+      pins[actualSlide].classList.remove("selected");
       ++actualSlide;
       if (actualSlide >= slides.length) {
         actualSlide = 0;
       }
       slides[actualSlide].classList.add("show");
+      pins[actualSlide].classList.add("selected");
       break;
 
     case "prev":
       slides[actualSlide].classList.remove("show");
+      pins[actualSlide].classList.remove("selected");
       --actualSlide;
       if (actualSlide < 0) {
         actualSlide = slides.length - 1;
       }
       slides[actualSlide].classList.add("show");
+      pins[actualSlide].classList.add("selected");
       break;
     default:
   }
@@ -45,7 +55,7 @@ const autoPlay = function (e) {
   if (playing) {
     playing = false;
     e.target.classList.add("on");
-    timer = setInterval(changeSlideByTimer, 3000);
+    timer = setInterval(changeSlideByArrow, 3000);
   } else {
     playing = true;
     e.target.classList.remove("on");
@@ -53,21 +63,29 @@ const autoPlay = function (e) {
   }
 };
 
-//function changing slides while play button clicked
-// TODO: how to pass a parameter to changeSlideByArrow function - now it throws a bug
-// because I refer to e. which in cSBA function is mouseEvent.
-const changeSlideByTimer = function () {
-  slides[actualSlide].classList.remove("show");
-  ++actualSlide;
-  if (actualSlide >= slides.length) {
-    actualSlide = 0;
-  }
-  slides[actualSlide].classList.add("show");
+// function changing slides while play button clicked
+
+// const changeSlideByTimer = function () {
+//   slides[actualSlide].classList.remove("show");
+//   ++actualSlide;
+//   if (actualSlide >= slides.length) {
+//     actualSlide = 0;
+//   }
+//   slides[actualSlide].classList.add("show");
+// };
+
+const executeOnLoad = function () {
+  pins[actualSlide].classList.remove("selected");
+  pins[0].classList.add("selected");
 };
+
+// executeOnLoad();
 
 pins.forEach((pin) => pin.addEventListener("click", changeSlideByPin));
 document.querySelector(".next").addEventListener("click", changeSlideByArrow);
 document.querySelector(".prev").addEventListener("click", changeSlideByArrow);
 document.querySelector("#play").addEventListener("click", autoPlay);
+
+document.addEventListener("onload", executeOnLoad());
 
 //workshop version
